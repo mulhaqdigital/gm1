a0caf8eb7f91b1b01f4ecf0df92ff5578e3cfbf8
sayaluqmanhakim <digital@mulhaq.org>
2026-03-11 02:56:20 +0800

    wow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Navbar } from "@/components/layout/Navbar";
+import { Navbar } from "@/components/layout/NavbarClient";
 import { Footer } from "@/components/layout/Footer";
 import { PageCard } from "@/components/pages/PageCard";
 import { db } from "@/db";
